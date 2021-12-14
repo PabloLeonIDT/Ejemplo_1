573a7f1ef7e8a26498e7bd3d97cffd94cd822bd1
Pablo <pabloleon46@gmail.com>
2021-12-14 11:22:36 -0600

    Se implementó una prueba de los sensores analógicos para verificar que estén funcionando.

---
+++ header_principal.h
@@ -0,0 +1,23 @@
+#define CAN_USE_EXTENDED_ID TRUE
+#define CAN_BRG_PRESCALAR           1    //Set CAN Baud Rate to 500K
+#define CAN_BRG_PHASE_SEGMENT_1     0    //Tq = (2(1+PRESCALAR))/(Fosc/2)
+#define CAN_BRG_PHASE_SEGMENT_2     0    //Tq = (2(1+4)/(20000000/2) = 0.000001
+#define CAN_BRG_PROPAGATION_TIME    0    //Baud Rate = 1/(((PHASE_SEGMENT_1+1)+(PHASE_SEGMENT_2+1)+(PROPAGATION_TIME+1)+(JUMP_WIDTH+1))*Tq)
+#define CAN_BRG_SYNCH_JUMP_WIDTH    0    //Baud Rate = 1/(((1+1)+(1+1)+(2+1)+(0+1))*0.000001) = 500000
+
+#include <18F4580.h>
+#device adc=10
+#device HIGH_INTS=TRUE 
+#FUSES NOWDT, HS, NOLVP
+#use delay(clock=20000000)
+#use RS232(BAUD=115200,BITS=8,PARITY=N,XMIT=PIN_C6,RCV=PIN_C7,errors)
+#include <can-18F4580.c>
+#include <crc_8.c>
+#ZERO_RAM  
+
+float LecturaVoltajePresion, LecturaPresion;
+float LecturaVoltajeFlujo;
+int16 LecturaFlujo;
+
+void PRUEBA_MALA_LECTURA(void);
+void RUTINA_PROBAR_SENSORES(void);
--- ventilamex_ejemplo_V1_0.c
+++ ventilamex_ejemplo_V1_0.c
@@ -1,25 +1,4 @@
-#define CAN_USE_EXTENDED_ID TRUE
-#define CAN_BRG_PRESCALAR           1    //Set CAN Baud Rate to 500K
-#define CAN_BRG_PHASE_SEGMENT_1     0    //Tq = (2(1+PRESCALAR))/(Fosc/2)
-#define CAN_BRG_PHASE_SEGMENT_2     0    //Tq = (2(1+4)/(20000000/2) = 0.000001
-#define CAN_BRG_PROPAGATION_TIME    0    //Baud Rate = 1/(((PHASE_SEGMENT_1+1)+(PHASE_SEGMENT_2+1)+(PROPAGATION_TIME+1)+(JUMP_WIDTH+1))*Tq)
-#define CAN_BRG_SYNCH_JUMP_WIDTH    0    //Baud Rate = 1/(((1+1)+(1+1)+(2+1)+(0+1))*0.000001) = 500000
-
-#include <18F4580.h>
-#device adc=10
-#device HIGH_INTS=TRUE 
-#FUSES NOWDT, HS, NOLVP
-#use delay(clock=20000000)
-#use RS232(BAUD=115200,BITS=8,PARITY=N,XMIT=PIN_C6,RCV=PIN_C7,errors)
-#include <can-18F4580.c>
-#include <crc_8.c>
-#ZERO_RAM  
-
-float LecturaVoltajePresion, LecturaPresion;
-float LecturaVoltajeFlujo;
-int16 LecturaFlujo;
-
-void PRUEBA_MALA_LECTURA(void);
+#include "header_principal.h"
 
 void main(){
    can_init(); 
@@ -29,6 +8,8 @@ void main(){
    enable_interrupts(INT_RDA);
    enable_interrupts(GLOBAL);
    
+   RUTINA_PROBAR_SENSORES();
+   
    while(TRUE){      
       
       PRUEBA_MALA_LECTURA();             
@@ -36,6 +17,23 @@ void main(){
       printf("%3.2f, %3.2Lu", LecturaPresion, LecturaFlujo);
    }
 
+}
+
+void RUTINA_PROBAR_SENSORES(void){
+   float PruebaVoltajeFlujo, PruebaVoltajePresion;
+   float PruebaFlujo, PruebaPresion;
+   
+   set_adc_channel(0);
+   delay_us(50);
+   PruebaVoltajeFlujo = read_adc();
+   PruebaFlujo = ((5.0*PruebaVoltajeFlujo/1024.0)*1.556)+0.5;            
+   
+   set_adc_channel(1);
+   delay_us(50);
+   PruebaVoltajePresion = read_adc();
+   PruebaPresion = ((5.0*PruebaVoltajePresion/1024.0));   
+   
+   
 }
 
 void PRUEBA_MALA_LECTURA(void){
